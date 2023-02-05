deecf59fc9ae682b35730c8c87537a89069e7ad1
Your Name <you@example.com>
2023-02-05 21:21:40 +0500

    git add tests

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -4,6 +4,7 @@ from tests.fixtures.open_result import open_file
 
 
 @pytest.mark.parametrize('file_1, file_2, expected',
-                         [('gendiff/file1.json', 'gendiff/file2.json', open_file('tests/fixtures/result'))])
-def test_tenerate_diff(file_1, file_2, expected):
+                         [('gendiff/file1.json', 'gendiff/file2.json',
+                           open_file('tests/fixtures/json_result'))])
+def test_generate_diff(file_1, file_2, expected):
     assert generate_diff(file_1, file_2) == expected
