aaa3091da04a9ead55fc13b6c89ca0157157bacf
Your Name <you@example.com>
2023-02-26 13:56:35 +0500

    git add gendiff

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -3,9 +3,19 @@ from gendiff.formatters.stylish_format import get_stylish
 from gendiff.formatters.plain_format import get_plain
 from gendiff.formatters.json_format import get_json
 
+from gendiff.open_file import open_file
+from gendiff.parse_file import parse_file
+from gendiff.get_file_extension import get_file_extension
+
 
 def generate_diff(file_1, file_2, file_format='stylish'):
-    diff_tree = make_diff(file_1, file_2)
+    opened_file_1, opened_file_2 = open_file(file_1), \
+        open_file(file_2)
+    extension_1, extension_2 = get_file_extension(file_1), \
+        get_file_extension(file_2)
+    parsed_file_1, parsed_file_2 = parse_file(opened_file_1, extension_1), \
+        parse_file(opened_file_2, extension_2)
+    diff_tree = make_diff(parsed_file_1, parsed_file_2)
     if file_format == 'plain':
         return get_plain(diff_tree)
     if file_format == 'json':
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,18 +1,10 @@
 from gendiff.cli import parse
 from gendiff import generate_diff
-from gendiff.open_file import open_file
-from gendiff.parse_file import parse_file
-from gendiff.get_file_extension import get_file_extension
+
 
 args = parse()
 
-opened_file_1, opened_file_2 = open_file(args.first_file), \
-    open_file(args.second_file)
-extension_1, extension_2 = get_file_extension(args.first_file), \
-    get_file_extension(args.second_file)
-parsed_file_1, parsed_file_2 = parse_file(opened_file_1, extension_1), \
-    parse_file(opened_file_2, extension_2)
-diff = generate_diff(parsed_file_1, parsed_file_2, args.format)
+diff = generate_diff(args.first_file, args.second_file, args.format)
 
 
 def main():
